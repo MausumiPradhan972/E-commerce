cfa4d48cf31086166605270f904e4fb3d8f64c40
Mausumi Pradhan <mausumip99@gmail.com>
2026-01-21 12:07:57 +0530

    initial commit

---
+++ assets/css/index.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//         $('#form').submit(function(e){
+//           e.preventDefault();
+//           $(".error").remove();
+//           let name=$('#name').val().trim();
+//           let email=$('#email').val().trim();
+//           let message=$('#message').val().trim();
+
+           
+//           let isValidForm=true;
+//           if(name===""){
+//             $('#name').after('<span class="error">This field is required</span>');
+//               isValidForm=false;
+//           }
+//           if(email===""){
+//             $('#email').after('<span class="error">This field is required</span>');
+//              isValidForm=false;
+//           }
+//           if(message===""){
+//             $('#message').after('<span class="error">This field is required</span>');
+//              isValidForm=false;
+//           }
+//          if(isValidForm){
+//           this.submit();
+//          }
+//         });
+//       })
+      
